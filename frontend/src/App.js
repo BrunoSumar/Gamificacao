204f09426a746810bf97baa15ebfcfa91689f6be
Emerson Teixeira <76541111+Emerson-Teixeira@users.noreply.github.com>
2022-02-28 11:07:51 -0300

    Pequenas mudanças front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,33 @@
 import React from "react";
 import GoogleLogin from "react-google-login";
-const responseGoogle = (response) => {
-  console.log(response);
+const fetchAccessTokenToServer = async (response) => {
+  let customHeader = new Headers();
+  try {
+    let responseFetch = await fetch(`${process.env.REACT_APP_SERVER_URL}login`, {
+      method: "POST",
+      headers: customHeader,
+      body: {
+        AccessToken: response.accessToken,
+      },
+    });
+    if (responseFetch.ok) {
+      let jsonResponse = await responseFetch.json();
+      console.log(jsonResponse || "a");
+    } else throw false;
+  } catch (error) {
+    alert("Error");
+  }
 };
 function App() {
   return (
-    <div className="App" style={{backgroundColor:'#f0f0f0'}}>
+    <div className="App" style={{ backgroundColor: "#f0f0f0" }}>
       <GoogleLogin
         clientId={process.env.REACT_APP_CLIENT_ID}
         buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
+        onSuccess={fetchAccessTokenToServer}
+        onFailure={(err) => alert(err)}
         cookiePolicy={"single_host_origin"}
-        responseType="code"
-        theme='dark'
+        theme="dark"
       />
     </div>
   );
