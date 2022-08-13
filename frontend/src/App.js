b400d7bc87388af63a712ebca5c4b8eead003103
Emerson Teixeira <76541111+Emerson-Teixeira@users.noreply.github.com>
2022-08-13 16:30:43 -0300

    Fix login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,67 +1,20 @@
-import React, { createContext } from "react";
-import { useGoogleLogin } from '@react-oauth/google';
-import { OAuth2Client } from "google-auth-library";
-import crypto from "crypto-js";
-const client = new OAuth2Client(
-  process.env.REACT_APP_CLIENT_ID,
-  process.env.REACT_APP_CLIENT_SECRET,
-  "http://localhost:3000"
-);
-
-const fetchAccessTokenToServer = async (response) => {
-  let customHeader = new Headers();
-  try {
-    let token = await getAuthAndRefreshToken(response.code);
-    console.log(token);
-    localStorage.setItem('googleTokens',crypto.AES.encrypt(
-      JSON.stringify({
-        access_token: token.access_token,
-        id_token: token.id_token,
-        refresh_token: token.refresh_token,
-        expiry_date:token.expiry_date
-      }),
-      process.env.REACT_APP_CRYPTO_CODE
-    ))
-    let responseFetch = await fetch(
-      `${process.env.REACT_APP_SERVER_URL}api/login/aluno`,
-      {
-        method: "POST",
-        headers: customHeader,
-        body: JSON.stringify({
-          id_token: token.id_token,
-          access_token: token.access_token,
-        }),
-      }
-    );
-    if (responseFetch.ok) {
-      let jsonResponse = await responseFetch.json();
-      console.log(jsonResponse);
-    } else throw false;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-async function getAuthAndRefreshToken(code) {
-  try {
-    const response = await client.getToken(code);
-    return response.tokens;
-  } catch (e) {
-    console.log(e);
-  }
-}
+import React, { useEffect } from "react";
 
 function App() {
-  const login = useGoogleLogin({
-    onSuccess: (codeResponse) => fetchAccessTokenToServer(codeResponse),
-    flow: "auth-code",
-    scopes:"https://www.googleapis.com/auth/classroom.courses.readonly"
+  function handleCallbackResponse(response) {
+    console.log(response);
+  }
+
+  /* global google*/
+  const client = google.accounts.oauth2.initTokenClient({
+    client_id: process.env.REACT_APP_CLIENT_ID,
+    scope: "https://www.googleapis.com/auth/classroom.courses.readonly",
+    callback: handleCallbackResponse,
   });
 
   return (
-    <div className="App" style={{ backgroundColor: "#f0f0f0" }}>g
-      {process.env.REACT_APP_CLIENT_ID}
-      <button onClick={() => login()} />
+    <div>
+      <button onClick={() => client.requestAccessToken()} id="signInDiv">Google Login</button>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 ReactDOM.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
