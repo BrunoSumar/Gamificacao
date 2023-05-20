1b18adda00788144fd50f82a3c4d6b05e90e4546
Bruno Sumar <sumar.bruno.34@gmail.com>
2023-05-20 16:17:49 -0300

    inicio rotina mais uma vez

--- backend/clean.js
+++ backend/clean.js
@@ -12,21 +12,22 @@ function cleanRoutine( pg ){
 
     const query_search = `SELECT "TXT_path_arquivo" FROM "Conteudos"`;
 
-    // const regex_conteudo = /^\.\/conteudos\/(.+)$/;
-    // const formatPath = rows => rows
-    //       .map( row => row.TXT_path_arquivo )
-    //       .map( path => path.match( regex_conteudo )[1] );
+    const regex_conteudo = /^\.\/conteudos\/(.+)$/;
+    const formatPaths = rows => rows
+          .map( row => row.TXT_path_arquivo )
+          .map( path => path.match( regex_conteudo )[1] );
 
     return async () => {
         // console.log( 'aaaaaaa' )
         // await pg.query( query_delete );
 
         const { rows } = await pg.query( query_search );
-        // const paths =
+        const paths = formatPaths( rows );
 
 
         const files = fs.readdirSync('./conteudos');
 
+        console.log( paths )
         console.log( files )
 
     };
