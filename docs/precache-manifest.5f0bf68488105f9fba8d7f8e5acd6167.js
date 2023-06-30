a4da21e6d2544d9e587e42e7a5f2492d7663d99e
Bruno Sumar <sumar.bruno.34@gmail.com>
2023-06-30 08:20:31 -0300

    Build 2

---
+++ docs/precache-manifest.5f0bf68488105f9fba8d7f8e5acd6167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc47e0c3410dd869ef97",
+    "url": "/Gamificacao/static/css/main.6f59015e.chunk.css"
+  },
+  {
+    "revision": "bc47e0c3410dd869ef97",
+    "url": "/Gamificacao/static/js/main.bc47e0c3.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "/Gamificacao/static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "1da02a466d3b9488ff44",
+    "url": "/Gamificacao/static/js/2.1da02a46.chunk.js"
+  },
+  {
+    "revision": "a865bf380ebdae62810f",
+    "url": "/Gamificacao/static/js/runtime~main.a865bf38.js"
+  },
+  {
+    "revision": "d9696bfc5038f30260527e968f702761",
+    "url": "/Gamificacao/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5718dbd14a5f0f14cd4daca2cbbf1d4c.js"
+  "/Gamificacao/precache-manifest.5f0bf68488105f9fba8d7f8e5acd6167.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Gamificacao/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
